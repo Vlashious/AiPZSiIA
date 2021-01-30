b02f2680ad3d138b7ae295ac790f9efa20a43711
Uładzimir Śniežka <flashro561@gmail.com>
2021-01-30 21:03:04 +0300

    simple udp client-server implementation

---
+++ Lab1/Lab1/Server/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var server = new UdpClient(4000);
+            var helloString = Encoding.UTF8.GetBytes("Hello Client!");
+            while (true)
+            {
+                var remoteEP = new IPEndPoint(IPAddress.Any, 4000);
+                var data = server.Receive(ref remoteEP);
+                Console.WriteLine($"<-- {remoteEP}: {data}");
+                server.Send(helloString, helloString.Length, remoteEP);
+            }
+        }
+    }
+}
