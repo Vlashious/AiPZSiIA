6a1c7056fd73b7c27c0c272ac2080b39832401af
Uładzimir Śniežka <flashro561@gmail.com>
2021-03-01 23:42:04 +0300

    publisher api written

---
+++ Lab3/API/Services/PublisherService.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using Models;
+using MongoDB.Driver;
+
+namespace Services
+{
+    public class PublisherService
+    {
+        private readonly IMongoCollection<Publisher> _publishers;
+
+        public PublisherService()
+        {
+            var client = new MongoClient();
+            var database = client.GetDatabase("aip");
+
+            _publishers = database.GetCollection<Publisher>("publishers");
+        }
+
+        public IEnumerable<Publisher> Get() => _publishers.Find(publisher => true).ToEnumerable();
+
+        public Publisher Get(string id) => _publishers.Find(publisher => publisher.Id == id).FirstOrDefault();
+
+        public Publisher Create(Publisher publisher)
+        {
+            _publishers.InsertOne(publisher);
+            return publisher;
+        }
+
+        public void Update(string id, Publisher publisherIn) => _publishers.ReplaceOne(publisher => publisher.Id == id, publisherIn);
+
+        public void Remove(string id) => _publishers.DeleteOne(publisher => publisher.Id == id);
+    }
+}
